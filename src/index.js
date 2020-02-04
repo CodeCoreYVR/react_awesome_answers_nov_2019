66602636c3f9de7b08a2d3399d51088c46cf084f
Hano <hinsul3113@gmail.com>
2020-02-04 10:36:30 -0800

    Add Question Show Page

--- src/index.js
+++ src/index.js
@@ -35,10 +35,20 @@ const AnswerDetails = () => {
   );
 };
 
+// QuestionShowPage component
+const QuestionShowPage = () => {
+  return (
+    <div>
+      <QuestionDetails />
+      <AnswerDetails />
+    </div>
+  );
+};
+
 // In JSX, self-closing tags must be closed. <img> doesn't work,
 // you must write <img />
 
-ReactDOM.render(<AnswerDetails />, document.getElementById("root"));
+ReactDOM.render(<QuestionShowPage />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
